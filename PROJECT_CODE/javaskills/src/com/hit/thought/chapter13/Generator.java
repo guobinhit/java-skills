ff6b7ff59722ba63e85593efab6829b11f3feefc
Charies Gavin <guobinhit@gmail.com>
2018-03-21 09:43:50 +0800

    add some code of chapter13

--- PROJECT_CODE/javaskills/src/com/hit/thought/chapter13/Generator.java
+++ PROJECT_CODE/javaskills/src/com/hit/thought/chapter13/Generator.java
@@ -2,7 +2,7 @@ package com.hit.thought.chapter13;
 
 /**
  * author:Charies Gavin
- * date:2018/3/12,9:15
+ * date:2018/3/19,9:15
  * https:github.com/guobinhit
  * description:生成器接口
  */
---
+++ PROJECT_CODE/javaskills/src/com/hit/thought/chapter13/SimpleHolder.java
@@ -0,0 +1,26 @@
+package com.hit.thought.chapter13;
+
+/**
+ * author:Charies Gavin
+ * date:2018/3/20,8:30
+ * https:github.com/guobinhit
+ * description:简单持有对象
+ */
+public class SimpleHolder {
+    private Object obj;
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public static void main(String[] args) {
+        SimpleHolder holder = new SimpleHolder();
+        holder.setObj("HelloWorld");
+        String s = (String) holder.getObj();
+        System.out.println(s);
+    }
+}

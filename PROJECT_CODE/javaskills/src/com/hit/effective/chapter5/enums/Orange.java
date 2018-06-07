d09e1e63ffd48dc712e953570a49ff523fd85c47
Charies Gavin <guobinhit@gmail.com>
2018-06-07 09:19:07 +0800

    add code of effective programming

---
+++ PROJECT_CODE/javaskills/src/com/hit/effective/chapter5/enums/Orange.java
@@ -0,0 +1,13 @@
+package com.hit.effective.chapter5.enums;
+
+/**
+ * author:Charies Gavin
+ * date:2018/6/6,8:36
+ * https:github.com/guobinhit
+ * description:枚举类型
+ */
+public enum Orange {
+    NAVEL,
+    TEMOLE,
+    BLOOD
+}

30a0c25582634c6b37bb838aeb58e990e9d75ed5
Charies Gavin <guobinhit@gmail.com>
2019-04-14 20:33:26 +0800

    DiningPhilosophers

---
+++ PCODE/javaskills/src/com/hit/thought/current/DiningPhilosophers.java
@@ -0,0 +1,167 @@
+package com.hit.thought.current;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * author:Charies Gavin
+ * date:2019/4/14,20:30
+ * https:github.com/guobinhit
+ * description:哲学家进餐问题
+ */
+public class DiningPhilosophers {
+    public static void main(String[] args) throws Exception {
+        // 死锁测试
+        deadlockingDiningPhilosophers(args);
+
+        // 破坏死锁
+//        fixedDiningPhilosophers(args);
+    }
+
+    private static void deadlockingDiningPhilosophers(String[] args) throws Exception {
+        int ponder = 3;
+        if (args.length > 0) {
+            ponder = Integer.parseInt(args[0]);
+        }
+
+        int size = 3;
+        if (args.length > 1) {
+            size = Integer.parseInt(args[1]);
+        }
+
+        ExecutorService executors = Executors.newCachedThreadPool();
+
+        Chopstick[] sticks = new Chopstick[size];
+
+        for (int i = 0; i < size; i++) {
+            sticks[i] = new Chopstick();
+        }
+
+        for (int i = 0; i < size; i++) {
+            executors.execute(new Philosopher(
+                    sticks[i], sticks[(i + 1) % size], i, ponder
+            ));
+        }
+
+        if (args.length == 3 && args[2].equals("timeout")) {
+            TimeUnit.SECONDS.sleep(5);
+        } else {
+            System.out.println("Press 'Enter' to quit");
+            System.in.read();
+        }
+        executors.shutdownNow();
+    }
+
+    private static void fixedDiningPhilosophers(String[] args) throws Exception {
+        int ponder = 3;
+        if (args.length > 0) {
+            ponder = Integer.parseInt(args[0]);
+        }
+
+        int size = 3;
+        if (args.length > 1) {
+            size = Integer.parseInt(args[1]);
+        }
+
+        ExecutorService executors = Executors.newCachedThreadPool();
+
+        Chopstick[] sticks = new Chopstick[size];
+
+        for (int i = 0; i < size; i++) {
+            sticks[i] = new Chopstick();
+        }
+
+        for (int i = 0; i < size; i++) {
+            // 破坏循环
+            if (i < (size - 1)) {
+                executors.execute(new Philosopher(
+                        sticks[i], sticks[i + 1], i, ponder
+                ));
+            } else {
+                executors.execute(new Philosopher(
+                        sticks[0], sticks[i], i, ponder
+                ));
+            }
+        }
+
+        if (args.length == 3 && args[2].equals("timeout")) {
+            TimeUnit.SECONDS.sleep(5);
+        } else {
+            System.out.println("Press 'Enter' to quit");
+            System.in.read();
+        }
+        executors.shutdownNow();
+    }
+}
+
+
+class Chopstick {
+    private boolean taken = false;
+
+    public synchronized void take() throws InterruptedException {
+        while (taken) {
+            wait();
+        }
+        taken = true;
+    }
+
+    public synchronized void drop() {
+        taken = false;
+        notifyAll();
+    }
+}
+
+class Philosopher implements Runnable {
+    private Chopstick left;
+    private Chopstick right;
+
+    private final int id;
+    private final int ponderFactor;
+
+    private Random rand = new Random(47);
+
+    private void pause() throws InterruptedException {
+        if (ponderFactor == 0) {
+            return;
+        }
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(ponderFactor * 250));
+    }
+
+    public Philosopher(Chopstick left, Chopstick right, int ident, int ponder) {
+        this.left = left;
+        this.right = right;
+        this.id = ident;
+        this.ponderFactor = ponder;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println(this + " " + "thinking");
+                pause();
+
+                System.out.println(this + " " + "grabbing right");
+                right.take();
+
+                System.out.println(this + " " + "grabbing left");
+                left.take();
+
+                System.out.println(this + " " + "eating");
+                pause();
+
+                right.drop();
+                left.drop();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(this + " " + "exiting via interrupt");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Philosopher " + id;
+    }
+}
---
+++ PCODE/javaskills/src/com/hit/thought/current/ToastOMatic.java
@@ -0,0 +1,177 @@
+package com.hit.thought.current;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * author:Charies Gavin
+ * date:2019/4/14,20:00
+ * https:github.com/guobinhit
+ * description:多线程制作吐司
+ */
+public class ToastOMatic {
+    public static void main(String[] args) throws InterruptedException {
+        ToastQueue dryQueue = new ToastQueue();
+        ToastQueue butteredQueue = new ToastQueue();
+        ToastQueue finishedQueue = new ToastQueue();
+
+        ExecutorService executors = Executors.newCachedThreadPool();
+
+        executors.execute(new Toaster(dryQueue));
+        executors.execute(new Butterer(dryQueue, butteredQueue));
+        executors.execute(new Jammer(butteredQueue, finishedQueue));
+        executors.execute(new Eater(finishedQueue));
+
+        TimeUnit.SECONDS.sleep(5);
+
+        executors.shutdownNow();
+    }
+}
+
+class Toast {
+    public enum Status {
+        DRY,
+        BUTTERED,
+        JAMMED
+    }
+
+    private Status status = Status.DRY;
+
+    private final int id;
+
+    public Toast(int idn) {
+        this.id = idn;
+    }
+
+    public void butter() {
+        status = Status.BUTTERED;
+    }
+
+    public void jam() {
+        status = Status.JAMMED;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Toast " + id + ": " + status;
+    }
+}
+
+class ToastQueue extends LinkedBlockingQueue<Toast> {
+}
+
+class Toaster implements Runnable {
+    private ToastQueue toastQueue;
+    private int count = 0;
+    private Random rand = new Random(47);
+
+    public Toaster(ToastQueue tq) {
+        this.toastQueue = tq;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(
+                        100 + rand.nextInt(500)
+                );
+                Toast t = new Toast(count++);
+                System.out.println(t);
+                toastQueue.put(t);
+            }
+
+
+        } catch (InterruptedException e) {
+            System.out.println("Toaster interrupted");
+        }
+        System.out.println("Toaster Off");
+    }
+}
+
+class Butterer implements Runnable {
+    private ToastQueue dryQueue, butteredQueue;
+
+    public Butterer(ToastQueue dry, ToastQueue buttered) {
+        this.dryQueue = dry;
+        this.butteredQueue = buttered;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                Toast t = dryQueue.take();
+                t.butter();
+                System.out.println(t);
+                butteredQueue.put(t);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Butterer interrupted");
+        }
+        System.out.println("Butterer Off");
+    }
+}
+
+class Jammer implements Runnable {
+    private ToastQueue butteredQueue, finishedQueue;
+
+    public Jammer(ToastQueue buttered, ToastQueue finished) {
+        this.butteredQueue = buttered;
+        this.finishedQueue = finished;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                Toast t = butteredQueue.take();
+                t.jam();
+                System.out.println(t);
+                finishedQueue.put(t);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Jammer interrupted");
+        }
+        System.out.println("Jammer Off");
+    }
+}
+
+class Eater implements Runnable {
+    private ToastQueue finishedQueue;
+    private int counter = 0;
+
+    public Eater(ToastQueue finished) {
+        this.finishedQueue = finished;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                Toast t = finishedQueue.take();
+                if (t.getId() != counter++ ||
+                        t.getStatus() != Toast.Status.JAMMED) {
+                    System.out.println(">>>> Error: " + t);
+                    System.exit(1);
+                } else {
+                    System.out.println("Chomp! " + t);
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Eater interrupted");
+        }
+        System.out.println("Eater Off");
+    }
+}
